497d91b2646450a7e31e042bc5f27f2248c02804
Stéphane Francel <stephane.francel@gmail.com>
2021-12-09 00:30:44 +0100

    Save theme-name in localStorage

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -11,26 +11,38 @@ export class ThemeService {
 
   private themeName!: ThemeName;
 
-  get name() {
+  get name(): ThemeName {
     return this.themeName;
   }
 
-  get altName() {
+  get altName(): ThemeName {
     return this.themeName === 'light' ? 'dark' : 'light';
   }
 
+  private readonly DEFAULT: ThemeName = 'dark';
+
   constructor(@Inject(DOCUMENT) private document: Document, private rendererFactory: RendererFactory2) {
-    this.setTheme();
+    this.restoreTheme();
   }
 
-  setTheme(name: ThemeName = 'dark') {
+  setTheme(name: ThemeName) {
     const oldName: ThemeName = name === 'light' ? 'dark' : 'light';
     this.renderer.removeClass(this.document.documentElement, `theme--${oldName}`);
     this.renderer.addClass(this.document.documentElement, `theme--${name}`);
     this.themeName = name;
+    this.storeTheme();
   }
 
   switchTheme() {
     this.setTheme(this.themeName === 'light' ? 'dark' : 'light');
+  }
+
+  private restoreTheme() {
+    const themeName = this.document.defaultView?.localStorage.getItem('app-theme');
+    this.setTheme(themeName as ThemeName || this.DEFAULT);
+  }
+
+  private storeTheme() {
+    this.document.defaultView?.localStorage.setItem('app-theme', this.themeName);
   }
 }
