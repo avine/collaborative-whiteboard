2e4c0a5573b723f62c18c7a650c3e0b1569da456
Stéphane Francel <stephane.francel@gmail.com>
2022-02-04 09:52:47 +0100

    Export pipes

--- projects/collaborative-whiteboard/src/public-api.ts
+++ projects/collaborative-whiteboard/src/public-api.ts
@@ -11,5 +11,6 @@ export * from './lib/cw.types';
 // Features
 export * from './lib/components';
 export * from './lib/directives';
+export * from './lib/pipes';
 export * from './lib/services';
 export * from './lib/utils';
