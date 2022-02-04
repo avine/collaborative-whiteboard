8d12a9290cb6e24ef41921f0654efe6a35155e48
Stéphane Francel <stephane.francel@gmail.com>
2022-02-04 07:25:18 +0100

    Build docs

--- src/app/app.version.ts
+++ src/app/app.version.ts
@@ -3,4 +3,4 @@ import { AppVersion } from './app.types';
 /**
  * App version (don't forget to update this value when releasing the app)
  */
-export const APP_VERSION: AppVersion = '2.0.0';
+export const APP_VERSION: AppVersion = '1.0.0';
